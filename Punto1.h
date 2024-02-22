fd3a0b7f9c733f920aab992c3fa687d188b0883b
LibardoAForeroL <libardoalexandersuccess@gmail.com>
2024-02-22 11:52:20 -0500

    Initial commit

---
+++ Punto1.h
@@ -0,0 +1,6 @@
+#ifndef PUNTO1_H
+#define PUNTO1_H
+
+int Ejercicio1();
+
+#endif // PUNTO1_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Punto1.h"
+
+using namespace std;
+
+int main()
+{
+    Ejercicio1();
+    return 0;
+}
